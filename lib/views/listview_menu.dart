ee5007cebfdced76b883b3cb949e38ab1543ce9c
samchat2561 <samchat2561@gmail.com>
2026-01-30 11:05:57 +0700

    update feture/listview menu

--- lib/views/listview_menu.dart
+++ lib/views/listview_menu.dart
@@ -1,5 +1,20 @@
 import 'package:flutter/material.dart';
 
+class ListMenu {
+  final String? title;
+  final String? subtitle;
+  final IconData? icon;
+
+  ListMenu({required this.title, required this.subtitle, required this.icon});
+}
+
+List<ListMenu> menus = [
+  ListMenu(title: "MENU-1", subtitle: "SUB-1", icon: Icons.person),
+  ListMenu(title: "MENU-2", subtitle: "SUB-2", icon: Icons.person_add),
+  ListMenu(title: "MENU-3", subtitle: "SUB-3", icon: Icons.wifi),
+  ListMenu(title: "MENU-4", subtitle: "SUB-4", icon: Icons.build),
+];
+
 class ListviewMenu extends StatelessWidget {
   const ListviewMenu({super.key});
 
@@ -15,12 +30,15 @@ class ListviewMenu extends StatelessWidget {
         child: ListView.separated(
           itemBuilder: (context, index) {
             return ListTile(
-              title: Text("MENU-1"),
-              subtitle: Text("SUB-1"),
-              leading: CircleAvatar(child: Icon(Icons.person)),
+              title: Text("${menus[index].title}"),
+              subtitle: Text("${menus[index].subtitle}"),
+              leading: CircleAvatar(child: Icon(menus[index].icon)),
               trailing: Icon(Icons.arrow_right),
               onTap: () {
-                debugPrint("MENU-1");
+                debugPrint(menus[index].title);
+                if (Navigator.canPop(context)) {
+                  Navigator.pop(context, menus[index].title);
+                }
               },
             );
           },
