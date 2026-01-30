c87a2856dad3fa1931495882b48479349a372d18
samchat2561 <samchat2561@gmail.com>
2026-01-30 11:53:24 +0700

    update feture/card_widget

--- lib/views/mycard.dart
+++ lib/views/mycard.dart
@@ -1,5 +1,19 @@
 import 'package:flutter/material.dart';
 
+class ListMenu {
+  final String? title;
+  final IconData? icon;
+  final Color? bkColor;
+
+  ListMenu({required this.title, required this.icon, required this.bkColor});
+}
+
+List<ListMenu> menus = [
+  ListMenu(title: "MENU-1", icon: Icons.person, bkColor: Colors.green),
+  ListMenu(title: "MENU-2", icon: Icons.person_add, bkColor: Colors.blue),
+  ListMenu(title: "MENU-3", icon: Icons.wifi, bkColor: Colors.red),
+];
+
 class Mycard extends StatelessWidget {
   const Mycard({super.key});
 
@@ -14,21 +28,26 @@ class Mycard extends StatelessWidget {
       body: Container(
         child: GridView.count(
           crossAxisCount: 3,
-          children: List.generate(3, (index) {
+          children: List.generate(menus.length, (index) {
             return Card(
+              color: menus[index].bkColor,
               margin: EdgeInsets.all(12.0),
               child: InkWell(
                 onTap: () {
-                  debugPrint("My Card");
+                  debugPrint(menus[index].title);
                 },
                 child: Center(
                   child: Column(
                     mainAxisSize: MainAxisSize.min,
                     children: [
-                      Icon(Icons.home, size: 40, color: Colors.blueAccent),
+                      Icon(
+                        menus[index].icon,
+                        size: 40,
+                        color: Colors.white,
+                      ),
                       Text(
-                        "Home",
-                        style: TextStyle(fontSize: 15, color: Colors.black),
+                        "${menus[index].title}",
+                        style: TextStyle(fontSize: 15, color: Colors.white),
                       ),
                     ],
                   ),
