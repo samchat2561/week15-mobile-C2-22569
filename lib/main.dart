213f072917bc9cf7753fb981b46574489f299321
samchat2561 <samchat2561@gmail.com>
2026-01-30 10:36:16 +0700

    update column widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_week15/views/row_page.dart';
+import 'package:flutter_week15/views/column_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,6 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: RowPage());
+    return MaterialApp(home: ColumnPage());
   }
 }
