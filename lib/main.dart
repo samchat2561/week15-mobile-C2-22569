97c138a95722b74e8505edf0d57c7953261a42fc
samchat2561 <samchat2561@gmail.com>
2026-01-30 10:52:26 +0700

    update feture/listview menu

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_week15/views/column_page.dart';
+import 'package:flutter_week15/views/listview_menu.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,6 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: ColumnPage());
+    return MaterialApp(home: ListviewMenu());
   }
 }
